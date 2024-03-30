27bf94d645550919595061e3cd7d9cd1bb07d499
H∆DI <hadi@thiqatech.com>
2024-03-30 23:52:36 +0400

    add validation and also update translation files

--- app/Http/Controllers/API/ComplaintController.php
+++ app/Http/Controllers/API/ComplaintController.php
@@ -11,22 +11,32 @@ use Illuminate\Support\Facades\Validator;
 
 class ComplaintController extends AppBaseController{
     
-    public function createComplaint(Request $request)
-    {
-
+    public function createComplaint(Request $request) {
         $request->validate([
             'post_no' => 'required|string',
-            'post_address' => 'nullable|string',
-            'type_of_fault' => 'nullable|string',
+            'post_address' => 'required|string',
+            'type_of_fault' => 'required|string',
             'date_of_complaint' => 'nullable|string',
             'complainant_name' => 'nullable|string',
             'complaint_rut' => 'nullable|string',
             'phone' => 'nullable|string',
             'image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:5256',
-            'comuna' => 'nullable|string',
-            'sector' => 'nullable|string',
-            'population' => 'nullable|string',
+            'comuna' => 'required|exists:comuna,id',
+            'sector' => 'required|exists:sectors,id',
+            'population' => 'required|exists:population,id',
         ]);
+
+        // Check if a similar complaint already exists in the database
+        $existingComplaint = Complaint::where([
+            'comuna' => $request->comuna,
+            'sector' => $request->sector,
+            'population' => $request->population,
+        ])->first();
+
+        // If a similar complaint exists, return a message
+        if ($existingComplaint) {
+            return $this->sendError(__('We have already received a complaint about this problem. Thank you for your feedback!'));
+        }
 
         $imagePath = null;
         if ($request->hasFile('image')) {
@@ -41,7 +51,7 @@ class ComplaintController extends AppBaseController{
         $data['status'] = "pending";
 
         Complaint::create($data);
-        return $this->sendSuccess('Complaint created successfully');
+        return $this->sendSuccess(__('Complaint created successfully'));
     }
 
     public function assignedComplaints(Request $request) {
